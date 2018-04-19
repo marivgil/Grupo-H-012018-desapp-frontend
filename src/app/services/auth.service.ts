1f839d2290569a8eaeb53fc73b13570d2efadc0b
@RedondaAnalia <a.redonda89@gmail.com>
2018-04-18 21:40:23 -0300

    fix prod

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -23,8 +23,7 @@ export class AuthService {
     public getProfile(cb): void {
     const accessToken = localStorage.getItem('access_token');
     if (!accessToken) {
-      throw new Error('Access Token must exist to fetch profile');
-    }
+      }
 
     const self = this;
     this.auth0.client.userInfo(accessToken, (err, profile) => {
