ec9f24599b3e434dd481fdbe309b2da67350892d
@RedondaAnalia <a.redonda89@gmail.com>
2018-05-03 17:39:56 -0300

    intento 7 heroku CORS

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+app.options('*', cors())
 
 // server.js
 const path = require('path');
