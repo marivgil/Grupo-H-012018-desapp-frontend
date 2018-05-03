b874fd7d919a08a7fe04188383e75bcacf2fa3ba
@RedondaAnalia <a.redonda89@gmail.com>
2018-05-03 18:07:48 -0300

    ..

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-var express = require('express')
-  , cors = require('cors')
-  , app = express();
+// server.js
+const express = require('express');
+var cors= require('cors');
 
-app.use(cors());
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
+// server.js
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.all('*', function(req, res, next) {
+  var origin = req.get('origin'); 
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.origin
