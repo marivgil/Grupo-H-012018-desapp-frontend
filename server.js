52028432fd5363142abb9409c502be582056f8f8
@RedondaAnalia <a.redonda89@gmail.com>
2018-05-03 17:10:00 -0300

    intento 4. CORS en Heroku

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors());
-
-app.get('/*', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
  
 app.listen(80, function () {
   console.log('CORS-enabled web server listening on port 80')
