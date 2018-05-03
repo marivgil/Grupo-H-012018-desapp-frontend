078e0fa2cac61ec8abe59f0dcd9027f9c64167bc
@RedondaAnalia <a.redonda89@gmail.com>
2018-05-03 16:44:32 -0300

    intento 2 heroku CORS

--- server.js
+++ server.js
@@ -2,10 +2,22 @@
 
 
 const express = require('express');
+const cors= require('cors');
 const app = express();
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -31,34 +43,4 @@ app.use(forceSSL());
 // ...
 
 // server.js
-const path = require('path');
-// ...
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-var cors = require('cors')
-
-app.use(cors());
-
-// Add headers
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next(); 
-});
+const path = require('path')
