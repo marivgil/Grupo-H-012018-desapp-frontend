0c11b927dc22ee18e4f75481707670444c65c0f1
@RedondaAnalia <a.redonda89@gmail.com>
2018-05-14 20:42:35 -0300

    Se actualiza la version http. Formulario de alta usuario

--- src/app/components/new-user/new-user.component.ts
+++ src/app/components/new-user/new-user.component.ts
@@ -13,16 +13,25 @@ export class NewUserComponent {
   forma:FormGroup;
 
   constructor() {
-    this.forma= new FormGroup({
-      'CUIL': new FormControl('', [Validators.required, Validators.pattern("[0-9]*")]),
-      'name': new FormControl('', Validators.required),
-      'surname': new FormControl('', Validators.required),
-      'address': new FormControl('', Validators.required),
-      'email': new FormControl('ejemplo@ejemplo.com.ar', Validators.required)//la validacion de email lo hace auth0!!!!
-    })
+    this.forma = new FormGroup({
+      'CUIL': new FormGroup({
+            'prefix': new FormControl  ('27', [Validators.required, Validators.pattern('[0-9]{2}')]),
+            'DNI': new FormControl     ('34940180', [Validators.required, Validators.pattern('[0-9]{8}')]),
+            'suffix': new FormControl  ('4', [Validators.required, Validators.pattern('[0-9]{1}')]),
+      }),
+      'name': new FormControl('Analia', Validators.required),
+      'surname': new FormControl('Redonda', Validators.required),
+      'address': new FormGroup({
+            'street': new FormControl  ('Arredondo', Validators.required),
+            'number': new FormControl  ('1238', [Validators.required, Validators.pattern('[0-9]*')]),
+      }),
+      'email': new FormControl('ejemplo@ejemplo.com.ar', Validators.required)// la validacion de email lo hace auth0!!!!
+    });
    }
 
-   registerMe(){}
+   registerMe() {
+     console.log(this.forma);
+   }
 
 
 }
