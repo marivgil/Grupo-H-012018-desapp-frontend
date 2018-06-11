14a273b0fd7db73d48d142da27e446c8dbb669c6
RedondaAnalia <a.redonda89@gmail.com>
2018-06-11 17:12:24 -0300

    Cambios en vista de editar usuario y editar auto

--- src/app/components/new-user/new-user.component.ts
+++ src/app/components/new-user/new-user.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
 
-
+declare var google;
 declare var $;
 @Component({
   selector: 'app-new-user',
@@ -14,8 +14,16 @@ declare var $;
 
 export class NewUserComponent implements OnInit {
 
+  zoom: number = 10;
+  // Start position
+  lat: number = -34.603418;
+  lng: number = -58.381592;
+  marker: any ;
+
   forma: FormGroup;
   user: any;
+  map: boolean = false;
+
 
   constructor(private _auth: AuthService,
               private _user: UserService,
@@ -29,28 +37,31 @@ export class NewUserComponent implements OnInit {
       }),
       'name': new FormControl('', Validators.required),
       'surname': new FormControl('', Validators.required),
-      'address': new FormGroup({
-            'street': new FormControl  ('', Validators.required),
-            'number': new FormControl  ('', [Validators.required, Validators.pattern('[0-9]*')]),
-      }),
+      'address': new FormControl('', Validators.required),
       'email': new FormControl('', Validators.required)// la validacion de email lo hace auth0!!!!
     });
-    console.log (this._auth.userBD.name);
 
-    this.forma.patchValue({
-        email: this._auth.userProfile.email,
-        name: this._auth.userBD.name,
-        surname: this._auth.userBD.surname
-    });
-    console.log( this._auth.userBD.cuil.toString().substring(10, 11));
-    this.forma.controls['CUIL'].patchValue({
-      prefix: this._auth.userBD.cuil.toString().substring(0, 2) ,
-      DNI: this._auth.userBD.cuil.toString().substring(2, 10),
-      suffix: this._auth.userBD.cuil.toString().substring(10, 11),
-    });
    }
 
-   ngOnInit() { }
+   ngOnInit() {
+    this.cargarForm();
+  }
+
+  cargarForm() {
+
+   this.forma.patchValue({
+       email: this._auth.userProfile.email,
+       name: this._auth.userBD.name,
+       surname: this._auth.userBD.surname,
+       address: this._auth.userBD.address
+   });
+   this.forma.controls['CUIL'].patchValue({
+     prefix: this._auth.userBD.cuil.toString().substring(0, 2) ,
+     DNI: this._auth.userBD.cuil.toString().substring(2, 10),
+     suffix: this._auth.userBD.cuil.toString().substring(10, 11),
+   });
+
+   }
 
    registerMe() {
 
@@ -59,7 +70,7 @@ export class NewUserComponent implements OnInit {
                this.forma.value.CUIL.suffix ;
 
     let user = {
-      address: this._auth.userBD.address,
+      address: this.forma.value.address,
       name: this.forma.value.name,
       status: this._auth.userBD.status,
       email: this._auth.userProfile.email,
@@ -73,6 +84,7 @@ export class NewUserComponent implements OnInit {
     this._user.editUser(user).subscribe(res => {
             this.modificarUsuarioBD(user);
             $('#nameModal').modal('hide');
+            this.marker = undefined;
     });
    }
 
@@ -80,5 +92,29 @@ export class NewUserComponent implements OnInit {
     this._auth.userBD.name = user.name;
     this._auth.userBD.surname = user.surname;
     this._auth.userBD.cuil = user.cuil;
+    this._auth.userBD.address = user.address;
+   }
+
+   openMap() {
+    this.map = true;
+   }
+
+   mapClicked($event: any) {
+     this.marker = {
+       lat: $event.coords.lat,
+       lng: $event.coords.lng,
+       draggable: false
+      };
+
+      let geocoder = new google.maps.Geocoder();
+      let origin1 = new google.maps.LatLng($event.coords.lat, $event.coords.lng);
+      geocoder.geocode( { location: origin1 } , (results, status) =>  {
+      if (status == 'OK') {
+        this.forma.patchValue({
+          address: results[0].formatted_address });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
    }
 }
