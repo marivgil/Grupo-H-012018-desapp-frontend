3db814c48b791e8450079d88ee2c5a8c87ec3268
RedondaAnalia <a.redonda89@gmail.com>
2018-06-29 18:19:33 -0300

    Formato reservas

--- src/app/components/reservations/reservations.component.ts
+++ src/app/components/reservations/reservations.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from '../../services/auth.service';
 import { UserService } from '../../services/user.service';
 import { PostsService } from '../../services/posts.service';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-reservations',
@@ -14,6 +15,8 @@ export class ReservationsComponent implements OnInit {
 
   post;
   reservation;
+  cost: number[];
+  scores: number[];
   url;
 
   constructor(private _reservations: ReservasService,
@@ -27,30 +30,49 @@ export class ReservationsComponent implements OnInit {
                   this._reservations.getAllPendingOwnerReservations(this._user.userProfile.email)
                                           .subscribe((res) => {
                                               this.reservation = res;
+                                              this.calculateCost(res);
+                                              this.calculateScores(res);
+                                              console.log(this.scores);
+                                              
                                               console.log(res);
-                                              if (res.lenght > 1) {
-                                                this._post.getPost(res[0].post).subscribe((log: any) => {
-                                                    this.post = log.json();
-                                                    console.log(log.json());
-                                                });
-                                              }
-
-                                          }); break;
+                                              });
+                                          break;
      case "/misReservas":
                   this._reservations.getAllPendingTenantReservations(this._user.userProfile.email)
                                           .subscribe((res) => {
                                               this.reservation = res;
+                                              this.calculateCost(res);
                                               console.log(res);
-                                              this._post.getPost(res[0].post).subscribe((log: any) => {
-                                                  this.post = log.json();
-                                                  console.log(log.json());
-                                              });
-                                          }); break;
+                                          });
+                                          break;
     }// fin switch
-
   }// fin constructor
 
   ngOnInit() {
+  }
+
+  calculateScores(res) {
+    this.scores = res.map((x) => {
+      switch (x.tenantUser.scores.length) {
+        case 0:
+          return 3;
+        default:
+          return this.calculateOneScore(x.tenantUser.scores);
+      }
+    });
+  }
+
+  calculateOneScore(scores) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return scores.reduce(reducer, 0);
+  }
+
+  calculateCost(res) {
+    this.cost = res.map(function(x) {
+      let fecha1 = moment(x.post.sinceDate);
+      let fecha2 = moment (x.post.untilDate);
+      return ((fecha2.diff(fecha1, 'days')) + 1) * x.post.costPerDay;
+     });
   }
 
   confirmReservation(id, idx) {
